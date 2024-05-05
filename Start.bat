e0ea34cb0b0161e1ff2570abd35ea297af66a612
Bunnywaffle <100540808+bunnywaffle@users.noreply.github.com>
2024-05-05 20:02:11 +0530

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,5 +1,13 @@
 @echo off
 
+:: Check if Gradio is installed
+echo Checking if Gradio is installed
+pip show gradio > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Gradio is not installed, installing it now...
+    pip install gradio
+)
+
 :: Run app.py
 echo Running app.py, Running...
 python app.py
